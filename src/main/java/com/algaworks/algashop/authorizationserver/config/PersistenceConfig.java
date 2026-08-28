042c31b05fe810165ed5f4c29f01dbfecb257a01
Breno Pedro Assis Tenório Pinto <brenopedro14@hotmail.com>
2026-08-28 20:52:40 -0300

    feat: integrando banco de dados para não perder dados ao reiniciar

---
+++ src/main/java/com/algaworks/algashop/authorizationserver/config/PersistenceConfig.java
@@ -0,0 +1,24 @@
+package com.algaworks.algashop.authorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+@Configuration
+public class PersistenceConfig {
+
+    @Bean
+    public JdbcOAuth2AuthorizationService auth2AuthorizationService(JdbcTemplate jdbcTemplate,
+                                                                    RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
+    }
+
+    @Bean
+    public JdbcOAuth2AuthorizationConsentService auth2AuthorizationConsentService(JdbcTemplate jdbcTemplate,
+                                                                                  RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
+    }
+}
