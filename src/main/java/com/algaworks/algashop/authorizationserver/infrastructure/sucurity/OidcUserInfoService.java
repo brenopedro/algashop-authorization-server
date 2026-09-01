9c56032f37e369732f2d2b96a5f888553effa666
Breno Pedro Assis Tenório Pinto <brenopedro14@hotmail.com>
2026-08-31 21:51:41 -0300

    feat: habilitando o openIde customizando-o

---
+++ src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/sucurity/OidcUserInfoService.java
@@ -0,0 +1,28 @@
+package com.algaworks.algashop.authorizationserver.infrastructure.sucurity;
+
+import com.algaworks.algashop.authorizationserver.domain.model.AuthUser;
+import com.algaworks.algashop.authorizationserver.domain.model.AuthUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OidcUserInfoService {
+
+    private final AuthUserRepository authUserRepository;
+
+    public OidcUserInfo loadUser(String email) {
+        AuthUser user = authUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + email));
+
+        return OidcUserInfo.builder()
+                .subject(user.getId().toString())
+                .name(user.getName())
+                .email(user.getEmail())
+                .claim("type", user.getType().name())
+                .claim("created_at", user.getCreatedAt().toEpochSecond())
+                .build();
+    }
+}
